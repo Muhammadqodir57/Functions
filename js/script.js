04d069f41f1713bf6318854ab5aedbd2c53bdc03
Muhammadqodir_57 <mirzamatovanasiba@gmail.com>
2024-01-06 19:38:31 +0500

    initial commit

---
+++ js/script.js
@@ -0,0 +1,301 @@
+//////////////// DECLORATION//////////////
+function log1() {
+    console.log('Log-1');
+}
+log1()
+
+function log2() {
+    console.log('Log-2');
+}
+log2()
+
+function log3() {
+    console.log('Log-3');
+}
+log3()
+
+function log4() {
+    console.log('Log-4');
+}
+log4()
+
+function log5() {
+    console.log('Log-5');
+}
+log5()
+
+function log6() {
+    console.log('Log-6');
+}
+log6()
+
+function log7() {
+    console.log('Log-7');
+}
+log7()
+
+function log8() {
+    console.log('Log-8');
+}
+log8()
+
+function log9() {
+    console.log('Log-9');
+}
+log9()
+
+function log10() {
+    console.log('Log-10');
+}
+log10()
+function log11() {
+    console.log('Log-11');
+}
+log11()
+function log12() {
+    console.log('Log-12');
+}
+log12()
+function log13() {
+    console.log('Log-13');
+}
+log13()
+function log14() {
+    console.log('Log-14');
+}
+log14()
+
+function log15() {
+    console.log('Log-15');
+}
+log15()
+
+function log16() {
+    console.log('Log-16');
+}
+log16()
+
+function log17() {
+    console.log('Log-17');
+}
+log17()
+
+function log18() {
+    console.log('Log-18');
+}
+log18()
+
+function log19() {
+    console.log('Log-19');
+}
+log19()
+
+function log11() {
+    console.log('Log-20');
+}
+function log20() {
+    console.log('Log-20');
+}
+log20()
+
+////////////////EXPRESSION//////////////
+const log21 = function() {
+   console.log('log-21');
+}
+log21()
+
+const log22 = function() {
+    console.log('log-22');
+ }
+ log22()
+
+ const log23 = function() {
+    console.log('log-23');
+ }
+ log23()
+
+ const log24 = function() {
+    console.log('log-24');
+ }
+ log24()
+  
+ const log25 = function() {
+    console.log('log-25');
+ }
+ log25()
+
+ const log26 = function() {
+    console.log('log-26');
+ }
+ log26()
+
+ const log27 = function() {
+    console.log('log-27');
+ }
+ log27()
+
+ const log28 = function() {
+    console.log('log-28');
+ }
+ log28()
+
+ const log29 = function() {
+    console.log('log-29');
+ }
+ log29()
+
+ const log30 = function() {
+    console.log('log-30');
+ }
+ log30()
+
+ const log31 = function() {
+    console.log('log-31');
+ }
+ log31()
+
+ const log32 = function() {
+    console.log('log-32');
+ }
+ log32()
+
+ const log33 = function() {
+    console.log('log-33');
+ }
+ log33()
+
+ const log34 = function() {
+    console.log('log-34');
+ }
+ log34()
+  
+ const log35 = function() {
+    console.log('log-35');
+ }
+ log35()
+
+ const log36 = function() {
+    console.log('log-36');
+ }
+ log36()
+
+ const log37 = function() {
+    console.log('log-37');
+ }
+ log37()
+
+ const log38 = function() {
+    console.log('log-38');
+ }
+ log38()
+
+ const log39 = function() {
+    console.log('log-39');
+ }
+ log39()
+
+ const log40 = function() {
+    console.log('log-40');
+ }
+ log40()
+
+////////////////ARROW//////////////
+const log41 = () => {
+    console.log('log-41');
+}
+log41()
+
+const log42 = () => {
+    console.log('log-42');
+}
+log42()
+
+const log43 = () => {
+    console.log('log-43');
+}
+log43()
+
+const log44 = () => {
+    console.log('log-44');
+}
+log44()
+
+const log45 = () => {
+    console.log('log-45');
+}
+log45()
+
+const log46 = () => {
+    console.log('log-46');
+}
+log46()
+
+const log47 = () => {
+    console.log('log-47');
+}
+log47()
+
+const log48 = () => {
+    console.log('log-48');
+}
+log48()
+
+const log49 = () => {
+    console.log('log-49');
+}
+log49()
+
+const log50 = () => {
+    console.log('log-50');
+}
+log50()
+
+const log51 = () => {
+    console.log('log-51');
+}
+log51()
+
+const log52 = () => {
+    console.log('log-52');
+}
+log52()
+
+const log53 = () => {
+    console.log('log-53');
+}
+log53()
+
+const log54 = () => {
+    console.log('log-54');
+}
+log54()
+
+const log55 = () => {
+    console.log('log-55');
+}
+log45()
+
+const log56 = () => {
+    console.log('log-56');
+}
+log56()
+
+const log57 = () => {
+    console.log('log-57');
+}
+log57()
+
+const log58 = () => {
+    console.log('log-58');
+}
+log58()
+
+const log59 = () => {
+    console.log('log-59');
+}
+log59()
+
+const log60 = () => {
+    console.log('log-60');
+}
+log60()
